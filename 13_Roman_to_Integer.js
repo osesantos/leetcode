67145712052555ec6f7a3acbc3b530b11cdd76ac
osesantos <jfnfsantos@gmail.com>
2023-12-30 19:30:36 +0000

    add 13

---
+++ 13_Roman_to_Integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  const mapping = new Map();
+  mapping.set("I", "1");
+  mapping.set("V", "5");
+  mapping.set("X", "10");
+  mapping.set("L", "50");
+  mapping.set("C", "100");
+  mapping.set("D", "500");
+  mapping.set("M", "1000");
+  
+  return s
+    .replace('IV', 'IIII')
+    .replace('IX', 'VIIII')
+    .replace('XL', 'XXXX')
+    .replace('XC', 'LXXXX')
+    .replace('CD', 'CCCC')
+    .replace('CM', 'DCCCC')
+    .split('').map(a => Number(mapping.get(a))).reduce((a, b) => a + b);
+};
+
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("LVIII")); // 58
+console.log(romanToInt("MCMXCIV")); // 1994
