940e8f50b4b9a2ea35e4ed6c9ea50f858f4692f0
osesantos <jfnfsantos@gmail.com>
2023-12-31 18:48:34 +0000

    add 14

--- 14_Longest_Common_Prefix.js
+++ 14_Longest_Common_Prefix.js
@@ -28,3 +28,32 @@ var longestCommonPrefix = function(strs) {
 console.log(longestCommonPrefix(["flower","flow","flight"])); // "fl"
 console.log(longestCommonPrefix(["dog","racecar","car"])); // ""
 console.log(longestCommonPrefix([""])); // ""
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix2 = function(strs) {
+  if (strs.length === 0 || strs[0] === '') {
+    return '';
+  }
+  let match = strs[0].split('')[0];
+  let counter = 0;
+  for (let i = 0; i < strs.length; i++) {
+    if (match === strs[i].split('')[0]) {
+      counter++;
+    }
+  }
+
+  if (counter === strs.length) {
+    const newStrs = strs.map(a => a.split('').slice(1).join(''));
+    return match + longestCommonPrefix2(newStrs);
+  } else {
+    return '';
+  }
+};
+
+console.log("-------------");
+console.log(longestCommonPrefix2(["flower","flow","flight"])); // "fl"
+console.log(longestCommonPrefix2(["dog","racecar","car"])); // ""
+console.log(longestCommonPrefix2([""])); // ""
